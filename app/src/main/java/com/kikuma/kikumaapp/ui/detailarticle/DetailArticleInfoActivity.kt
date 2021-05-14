33a38f34c395c7d582e7cc859ecf8f2cb018c0a4
Mumti Hany Farisa <mumti.hany18@mhs.uinjkt.ac.id>
2021-05-14 11:50:54 +0700

    first

---
+++ app/src/main/java/com/kikuma/kikumaapp/ui/detailarticle/DetailArticleInfoActivity.kt
@@ -0,0 +1,12 @@
+package com.kikuma.kikumaapp.ui.detailarticle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kikuma.kikumaapp.R
+
+class DetailArticleInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_article_info)
+    }
+}
