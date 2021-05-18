18f8bec1035f2fb61749fa49e8d9bc71045d616a
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-05-18 22:38:23 +0700

    add camera function tocamera page

--- app/src/main/java/com/kikuma/kikumaapp/MainActivity.kt
+++ app/src/main/java/com/kikuma/kikumaapp/MainActivity.kt
@@ -1,19 +1,36 @@
 package com.kikuma.kikumaapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.ViewTreeObserver
+import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.marginBottom
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kikuma.kikumaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val activityMainBinding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val navView: BottomNavigationView = activityMainBinding.navView
+        setContentView(activityMainBinding.root)
+//
+//        val viewTreeObserver: ViewTreeObserver = navView.viewTreeObserver
+//        if(viewTreeObserver.isAlive){
+//            viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
+//                override fun onGlobalLayout() {
+//                    val viewHeight: Int = navView.height
+//                    if (viewHeight != 0) navView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+//                    val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+//                    navHostFragment?.view?.marginBottom = viewHeight
+//                }
+//            })
+//        }
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
