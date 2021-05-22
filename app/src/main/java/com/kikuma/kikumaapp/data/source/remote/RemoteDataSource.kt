40ff15a3b7029b4f48c83781761604b91147b965
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-05-23 01:18:00 +0700

    Article: Integrate article with Firestore

--- app/src/main/java/com/kikuma/kikumaapp/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/kikuma/kikumaapp/data/source/remote/RemoteDataSource.kt
@@ -1,13 +1,18 @@
 package com.kikuma.kikumaapp.data.source.remote
 
+import android.content.ContentValues.TAG
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import com.kikuma.kikumaapp.data.source.remote.response.ArticleResponse
 import com.kikuma.kikumaapp.data.source.remote.response.HistoryResponse
 import com.kikuma.kikumaapp.utils.EspressoIdlingResource
 import com.kikuma.kikumaapp.utils.JsonHelper
+import kotlin.reflect.typeOf
 
 class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
 
@@ -25,13 +30,40 @@ class RemoteDataSource private constructor(private val jsonHelper: JsonHelper) {
                 }
     }
 
+    private fun firestoreInstance(): FirebaseFirestore {
+        // Access a Cloud Firestore instance from your Activity
+        val db = FirebaseFirestore.getInstance()
+//        val articleCollectionRef = db.collection("articles")
+        return db
+    }
+
     fun getAllArticles(): LiveData<ApiResponse<List<ArticleResponse>>> {
-        EspressoIdlingResource.increment()
         val resultArticle = MutableLiveData<ApiResponse<List<ArticleResponse>>>()
-        handler.postDelayed({
-            resultArticle.value = ApiResponse.success(jsonHelper.loadArticles())
-            EspressoIdlingResource.decrement()
-        }, SERVICE_LATENCY_IN_MILLIS)
+        val articleRef = firestoreInstance().collection("articles")
+        articleRef.get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("wkwk", " => " + task.result)
+                    val documents = task.result?.documents
+                    if(documents != null){
+                        val articleList = ArrayList<ArticleResponse>()
+                        for (response in documents) {
+                            Log.d("ini poster", response.id + " => " + response.data?.get("imagePath"))
+                            val document = ArticleResponse(
+                                response.id,
+                                response.data?.get("title").toString(),
+                                response.data?.get("description").toString(),
+                                response.data?.get("imagePath").toString(),
+                                response.data?.get("posted").toString(),
+                            )
+                            articleList.add(document)
+                        }
+                        resultArticle.value = ApiResponse.success(articleList)
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.exception)
+                }
+            }
         return resultArticle
     }
 
