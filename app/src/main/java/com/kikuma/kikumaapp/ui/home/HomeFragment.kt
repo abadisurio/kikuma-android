80eaf480e6320957386ba1635c6fc65d8e2ffbe8
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-05-25 22:14:29 +0700

    FragmentHome: Add auth name to greetings

--- app/src/main/java/com/kikuma/kikumaapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/kikuma/kikumaapp/ui/home/HomeFragment.kt
@@ -4,22 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.kikuma.kikumaapp.R
-import com.kikuma.kikumaapp.databinding.ActivitySignUpBinding
 import com.kikuma.kikumaapp.databinding.FragmentHomeBinding
-import com.kikuma.kikumaapp.utils.DataDummy
 import com.kikuma.kikumaapp.viewmodel.ViewModelFactory
 import com.kikuma.kikumaapp.vo.Status
 
 class HomeFragment : Fragment() {
 
     private lateinit var fragmentHomeBinding: FragmentHomeBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -27,6 +27,7 @@ class HomeFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View {
         fragmentHomeBinding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        auth = Firebase.auth
         return fragmentHomeBinding.root
     }
 
@@ -60,5 +61,8 @@ class HomeFragment : Fragment() {
             this.setHasFixedSize(false)
             this.adapter = homeAdapter
         }
+
+        fragmentHomeBinding.tvName.text = resources.getString(R.string.hello , auth.currentUser?.displayName)
+
     }
 }
